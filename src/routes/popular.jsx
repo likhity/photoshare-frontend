a4312ac83109749f859d38c6d879d9a27a59ed42
Abhig2002 <abhig2002@gmail.com>
2023-04-23 00:59:20 -0700

    partially finished popular stuff page

--- src/routes/popular.jsx
+++ src/routes/popular.jsx
@@ -1,20 +1,121 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import "./css/popular.css";
-export default function popular() {
 
-    useEffect()
+function PopularTags() {
+  const [popularTags, setPopularTags] = useState([]);
 
-    return (
-    <div className="container">
-        <div className="grid-section">
-        <h2>Popular Tags</h2>
-        
-        </div>
+  useEffect(() => {
+    async function fetchPopularTags() {
+      try {
+        const response = await axios.get('/api/popular-tags');
+        setPopularTags(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchPopularTags();
+  }, []);
 
-        <div className="list-section">
-        <h2>Top 10 Users</h2>
-        
-        </div>
+  return (
+    <div>
+      <h2>Popular Tags</h2>
+      <ul>
+        {popularTags.map((tag) => (
+          <li key={tag.tag}>
+            <Link to={{ pathname: '/', search: `?tags=${tag.tag}` }}>
+              {tag.tag} ({tag.numPosts})
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
-    )
+  );
 }
+
+function TopUsers() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+      axios.get("/api/top-10-users").then((response) => {
+        setUsers(response.data);
+      });
+    }, []);
+    
+  return (
+    <div>
+      <h2>Top Users</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Contributions</th>
+          </tr>
+        </thead>
+        <tbody>
+        {users.map((user) => (
+            <tr key={user.userId}>
+              <td>{user.userId}</td>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.contributions}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function PhotoRecommendations() {
+  const [photoRecommendations, setPhotoRecommendations] = useState([]);
+
+  useEffect(() => {
+    async function fetchPhotoRecommendations() {
+      try {
+        const response = await axios.get('/api/photo-recommendations');
+        setPhotoRecommendations(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchPhotoRecommendations();
+  }, []);
+
+  return (
+    <div>
+      <h2>Photo Recommendations</h2>
+      <ul>
+        {photoRecommendations.map((photo) => (
+          <li key={photo.photoId}>
+            <img src={photo.filepath} alt={photo.caption} width="200" height="200" />
+            <p>{photo.caption}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function popular() {
+  const params = new URLSearchParams(window.location.search);
+  const tag = params.get('tag');
+
+  return (
+    <div>
+      {tag ? (
+        <h1>Showing photos tagged with "{tag}"</h1>
+      ) : (
+        <h1>Welcome to our photo sharing app!</h1>
+      )}
+      <PopularTags />
+      <TopUsers />
+      <PhotoRecommendations />
+    </div>
+  );
+}
+
+export default popular;
