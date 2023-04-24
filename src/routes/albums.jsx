5d3d96820f787ffde21779130c61ec958f04d394
Joaquin Uribe <joaquin.uribe.acosta@gmail.com>
2023-04-23 21:46:46 -0700

    PSF-6, made it so you can create albums

--- src/routes/albums.jsx
+++ src/routes/albums.jsx
@@ -5,8 +5,38 @@ import { getUser } from "../utils";
 
 import "./css/user-albums.css";
 
+function CreateAlbumBox({ onClose, onCreate }) {
+  const [newAlbumName, setNewAlbumName] = useState('');
+
+  const handleNewAlbumNameChange = (event) => {
+    setNewAlbumName(event.target.value);
+  };
+
+  const handleCreateAlbum = () => {
+    onCreate(newAlbumName);
+    setNewAlbumName('');
+  };
+
+  return (
+    <div className="create-album-box">
+      <div className="create-album-header">
+        <h2>New Album</h2>
+        <button className="create-album-close" onClick={onClose}>&times;</button>
+      </div>
+      <div className="create-album-body">
+        <label htmlFor="new-album-name">Name:</label>
+        <input type="text" id="new-album-name" value={newAlbumName} onChange={handleNewAlbumNameChange} />
+      </div>
+      <div className="create-album-footer">
+        <button className="create-album-create" onClick={handleCreateAlbum}>Create</button>
+      </div>
+    </div>
+  );
+}
+
 function AlbumList({ userId }) {
   const [albums, setAlbums] = useState([]);
+  const [showCreateAlbum, setShowCreateAlbum] = useState(false);
   const params = useParams();
   const loggedInUser = getUser();
 
@@ -17,10 +47,34 @@ function AlbumList({ userId }) {
       .catch((error) => console.error(error));
   }, [params.userId]);
 
+  const handleCreateAlbumClick = () => {
+    setShowCreateAlbum(true);
+  };
+
+  const handleCreateAlbum = (albumName) => {
+    axios.post(`/api/create-album?name=${albumName}`)
+    .then(() => {
+      axios.get(`/api/albums?userId=${params.userId}`)
+      .then((response) => setAlbums(response.data))
+      .catch((error) => console.error(error));
+      setShowCreateAlbum(false);
+    })
+    .catch((error) => console.error(error));
+  };
+
+  const handleCancelCreateAlbum = () => {
+    setShowCreateAlbum(false);
+  };
+
   return (
     <div className="user-albums">
       <h1>Albums</h1>
-      {loggedInUser && loggedInUser.userId === parseInt(params.userId) && <button className='create_album-button' onClick={() => { }}>Create Album</button>}
+      {loggedInUser && loggedInUser.userId === parseInt(params.userId) && <button className='create_album-button' onClick={handleCreateAlbumClick}>Create Album</button>}
+      {showCreateAlbum && (
+        <div className="create-album-overlay">
+          <CreateAlbumBox onClose={handleCancelCreateAlbum} onCreate={handleCreateAlbum} />
+        </div>
+      )}
       <div className="album-grid">
         {albums.map((album) => (
           <Link to={`/u/${params.userId}/albums/${encodeURIComponent(album.albumName)}`} key={album.albumId}>
